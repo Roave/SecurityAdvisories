64203be9207ec5ae5bec1474234b5d7e937d25a2
Marco Pivetta <ocramius@gmail.com>
2014-12-11 01:21:43 +0100

    Basic test for the `Advisory` VO

---
+++ build-advisories/test/RoaveTest/SecurityAdvisories/AdvisoryTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace RoaveTest\SecurityAdvisories;
+
+use PHPUnit_Framework_TestCase;
+use Roave\SecurityAdvisories\Advisory;
+
+class AdvisoryTest extends PHPUnit_Framework_TestCase
+{
+    public function testFromArrayWithValidConfig()
+    {
+        $advisory = Advisory::fromArrayData([
+            'reference' => 'composer://foo/bar',
+            'branches' => [
+                '1.0.x' => [
+                    'versions' => ['>=1.0', '<1.1'],
+                ],
+                '2.0.x' => [
+                    'versions' => ['>=2.0', '<2.1'],
+                ],
+            ],
+        ]);
+
+        $this->assertInstanceOf(Advisory::class, $advisory);
+
+        $this->assertSame('foo/bar', $advisory->getComponentName());
+        $this->assertSame('>=1.0,<1.1|>=2.0,<2.1', $advisory->getConstraint());
+    }
+}
