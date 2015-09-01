233f11346020fb0a6a6fc3c8d6cf034529f262c1
Marco Pivetta <ocramius@gmail.com>
2015-09-02 00:23:42 +0100

    Basic version constraint tests

---
+++ build-advisories/test/RoaveTest/SecurityAdvisories/VersionConstraintTest.php
@@ -0,0 +1,78 @@
+<?php
+/*
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * This software consists of voluntary contributions made by many individuals
+ * and is licensed under the MIT license.
+ */
+
+namespace RoaveTest\SecurityAdvisories;
+
+use PHPUnit_Framework_TestCase;
+use Roave\SecurityAdvisories\Advisory;
+use Roave\SecurityAdvisories\Component;
+use Roave\SecurityAdvisories\VersionConstraint;
+
+/**
+ * Tests for {@see \Roave\SecurityAdvisories\VersionConstraint}
+ *
+ * @covers \Roave\SecurityAdvisories\VersionConstraint
+ */
+class VersionConstraintTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @dataProvider closedRangesProvider
+     *
+     * @param string $stringConstraint
+     */
+    public function testFromRange($stringConstraint)
+    {
+        $constraint = VersionConstraint::fromString($stringConstraint);
+
+        $this->assertInstanceOf(VersionConstraint::class, $constraint);
+        $this->assertTrue($constraint->isSimpleRangeString());
+        $this->assertSame($stringConstraint, $constraint->getConstraintString());
+        $this->assertRegExp('/(\d+.)*\d+/', $constraint->getLowerBound());
+        $this->assertRegExp('/(\d+.)*\d+/', $constraint->getUpperBound());
+
+        $this->assertSame((bool) preg_match('/>=/', $stringConstraint), $constraint->isLowerBoundIncluded());
+        $this->assertSame((bool) preg_match('/<=/',$stringConstraint), $constraint->isUpperBoundIncluded());
+    }
+
+    /**
+     * @return string[][]
+     */
+    public function closedRangesProvider()
+    {
+        $matchedRanges = [
+            ['>1.2.3,<4.5.6'],
+            ['>=1.2.3,<4.5.6'],
+            ['>1.2.3,<=4.5.6'],
+            ['>=1.2.3,<=4.5.6'],
+            ['>  1.2.3  , < 4.5.6'],
+            ['>=  1.2.3  , <4.5.6'],
+            ['> 1.2.3 , <=4.5.6'],
+            ['>=1.2.3, <=4.5.6'],
+        ];
+
+        return array_combine(
+            array_map(
+                function (array $entry) {
+                    return $entry[0];
+                },
+                $matchedRanges
+            ),
+            $matchedRanges
+        );
+    }
+}
