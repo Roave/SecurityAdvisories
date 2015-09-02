fb3d811baa1cc5b09f0b38dc22ecfd95bd7fdbc0
Marco Pivetta <ocramius@gmail.com>
2015-09-02 01:05:27 +0100

    Testing `Version` with a valid string provider

--- build-advisories/test/RoaveTest/SecurityAdvisories/VersionTest.php
+++ build-advisories/test/RoaveTest/SecurityAdvisories/VersionTest.php
@@ -42,6 +42,31 @@ class VersionTest extends PHPUnit_Framework_TestCase
 
         Version::fromString($versionString);
     }
+    /**
+     * @dataProvider validVersionStringProvider
+     *
+     * @param string $versionString
+     */
+    public function testGetVersionWithValidVersion($versionString)
+    {
+        $version = Version::fromString($versionString);
+
+        $this->assertInstanceOf(Version::class, $version);
+        $this->assertSame($versionString, $version->getVersion());
+    }
+
+    public function validVersionStringProvider()
+    {
+        return [
+            ['0'],
+            ['1'],
+            ['12345'],
+            ['0.1.2.3.4'],
+            ['1.2.3.4'],
+            ['1.2.3.4.5.6.7.8.9.10'],
+            ['12345.12345.12345.12345.0'],
+        ];
+    }
 
     public function invalidVersionStringsProvider()
     {
