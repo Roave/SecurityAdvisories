f48ac7a0ae93f13f253c969db3502062da13e801
Marco Pivetta <ocramius@gmail.com>
2015-12-13 00:11:48 +0100

    And of course bash bites back! Have I ever written 2 lines of code without at least one mistake in them?

--- build-advisories/update-security-advisories.php
+++ build-advisories/update-security-advisories.php
@@ -48,7 +48,7 @@ use UnexpectedValueException;
     $execute = function ($commandString) {
         // may the gods forgive me for this inlined command addendum, but I CBA to fix proc_open's handling
         // of exit codes.
-        exec($commandString . ' 2&>1', $output, $result);
+        exec($commandString . ' 2>&1', $output, $result);
 
         if (0 !== $result) {
             throw new \UnexpectedValueException(sprintf(
