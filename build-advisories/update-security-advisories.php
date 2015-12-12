7454b4545b4f5c146d9b4b108f713206796f25ba
Marco Pivetta <ocramius@gmail.com>
2015-12-12 23:05:20 +0100

    Running composer installation as part of the hook logic

--- build-advisories/update-security-advisories.php
+++ build-advisories/update-security-advisories.php
@@ -21,45 +21,62 @@ namespace Roave\SecurityAdvisories;
 use ErrorException;
 use UnexpectedValueException;
 
-require_once __DIR__ . '/vendor/autoload.php';
-
-set_error_handler(
-    function ($errorCode, $message = '', $file = '', $line = 0) {
-        throw new ErrorException($message, 0, $errorCode, $file, $line);
-    },
-    E_STRICT | E_NOTICE | E_WARNING
-);
-
 // Note: this script is responsible for handling incoming requests from the github push notifications,
 // and to re-run the code generation/checks every time
+(function () {
+    require_once __DIR__ . '/vendor/autoload.php';
 
-$runInPath = function (callable $function, $path) {
-    $originalPath = getcwd();
+    set_error_handler(
+        function ($errorCode, $message = '', $file = '', $line = 0) {
+            throw new ErrorException($message, 0, $errorCode, $file, $line);
+        },
+        E_STRICT | E_NOTICE | E_WARNING
+    );
 
-    chdir($path);
+    $runInPath = function (callable $function, $path) {
+        $originalPath = getcwd();
 
-    try {
-        $returnValue = $function();
-    } finally {
-        chdir($originalPath);
-    }
+        chdir($path);
 
-    return $returnValue;
-};
+        try {
+            $returnValue = $function();
+        } finally {
+            chdir($originalPath);
+        }
 
-$execute = function ($commandString) {
-    exec($commandString, $output, $result);
+        return $returnValue;
+    };
 
-    if (0 !== $result) {
-        throw new \UnexpectedValueException(sprintf('Command failed: "%s" "%s"', $commandString, $output));
-    }
-};
+    $execute = function ($commandString) {
+        exec($commandString, $output, $result);
 
-(function () use ($execute, $runInPath) {
+        if (0 !== $result) {
+            throw new \UnexpectedValueException(sprintf(
+                'Command failed: "%s" "%s"',
+                $commandString,
+                implode(PHP_EOL, $output)
+            ));
+        }
+    };
+
     $runInPath(
         function () use ($execute) {
             $execute('git fetch origin');
             $execute('git reset --hard origin/master');
+        },
+        realpath(__DIR__ . '/..')
+    );
+
+    $runInPath(
+        function () use ($execute) {
+            $execute(sprintf('curl -sS https://getcomposer.org/installer | %s', escapeshellarg(PHP_BINARY)));
+            $execute(sprintf('%s composer.phar install', escapeshellarg(PHP_BINARY)));
+        },
+        realpath(__DIR__)
+    );
+
+    $runInPath(
+        function () use ($execute) {
             $execute('php build-advisories/build-conflicts.php');
             $execute('git push origin master');
         },
