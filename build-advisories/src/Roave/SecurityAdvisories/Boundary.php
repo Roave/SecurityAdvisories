7a7f02fa1b1ef2a260ca7ecc0b8a615f52aae668
Marco Pivetta <ocramius@gmail.com>
2016-09-15 01:50:42 +0100

    Basic implementation of `Boundary` as per specification

---
+++ build-advisories/src/Roave/SecurityAdvisories/Boundary.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Roave\SecurityAdvisories;
+
+/**
+ * A simple version, such as 1.0 or 1.0.0.0 or 2.0.1.3.2
+ */
+final class Boundary
+{
+    private const MATCHER = '/^(<|>)(=?)\s*((?:\d+\.)*\d+)$/';
+
+    /**
+     * @var Version
+     */
+    private $version;
+
+    /**
+     * @var string one of "<", "<=", "=", ">=", ">"
+     */
+    private $limitType;
+
+    private function __construct(Version $version, string $limitType)
+    {
+        $this->version   = $version;
+        $this->limitType = $limitType;
+    }
+
+    /**
+     * @param string $boundary
+     *
+     * @return Boundary
+     *
+     * @throws \InvalidArgumentException
+     */
+    public static function fromString(string $boundary) : self
+    {
+        if (! preg_match(self::MATCHER, $boundary, $matches)) {
+            throw new \InvalidArgumentException(sprintf('The given string "%s" is not a valid boundary', $boundary));
+        }
+
+        return new self(
+            Version::fromString($matches[3]),
+            $matches[1] . $matches[2]
+        );
+    }
+}
