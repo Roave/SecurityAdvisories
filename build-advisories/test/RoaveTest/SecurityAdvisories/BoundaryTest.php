3ad4f505786b3f6697fbd9953d971e066f3038f0
Marco Pivetta <ocramius@gmail.com>
2016-09-15 01:50:24 +0100

    Basic spec for a `Boundary` object that abstracts a limit

---
+++ build-advisories/test/RoaveTest/SecurityAdvisories/BoundaryTest.php
@@ -0,0 +1,78 @@
+<?php
+/*
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * This software consists of voluntary contributions made by many individuals
+ * and is licensed under the MIT license.
+ */
+
+declare(strict_types=1);
+
+namespace RoaveTest\SecurityAdvisories;
+
+use PHPUnit_Framework_TestCase;
+use Roave\SecurityAdvisories\Boundary;
+use Roave\SecurityAdvisories\Version;
+
+/**
+ * Tests for {@see \Roave\SecurityAdvisories\Boundary}
+ *
+ * @covers \Roave\SecurityAdvisories\Boundary
+ */
+class BoundaryTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @dataProvider invalidBoundaryStrings
+     *
+     * @param string $boundaryString
+     *
+     * @return void
+     */
+    public function testRejectsInvalidBoundaryStrings(string $boundaryString) : void
+    {
+        $this->setExpectedException(\InvalidArgumentException::class);
+
+        Boundary::fromString($boundaryString);
+    }
+
+    /**
+     * @return string[][]
+     */
+    public function invalidBoundaryStrings() : array
+    {
+        return [
+            [''],
+            ['foo'],
+            ['1'],
+            ['1.2.3'],
+            ['1.2.3='],
+            ['1.2.3<='],
+            ['1.2.3<'],
+            ['1.2.3>'],
+            ['1.2.3>='],
+            ['<'],
+            ['>'],
+            ['<='],
+            ['>='],
+            ['='],
+            ['=='],
+            ['><'],
+            ['<>'],
+            ['=>'],
+            ['=<'],
+            ['=>1.2'],
+            ['=<1.2'],
+            ['1.2'],
+        ];
+    }
+}
