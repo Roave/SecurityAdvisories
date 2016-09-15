d402bef180c80af7f4d567e180539f187bce52e4
Marco Pivetta <ocramius@gmail.com>
2016-09-15 01:28:26 +0100

    Using coalesce operator where it makes sense

--- build-advisories/src/Roave/SecurityAdvisories/Version.php
+++ build-advisories/src/Roave/SecurityAdvisories/Version.php
@@ -86,7 +86,7 @@ final class Version
     public function isGreaterOrEqualThan(self $other) : bool
     {
         foreach ($other->versionNumbers as $index => $otherVersion) {
-            $thisVersion = isset($this->versionNumbers[$index]) ? $this->versionNumbers[$index] : 0;
+            $thisVersion = $this->versionNumbers[$index] ?? 0;
 
             if ($thisVersion === $otherVersion) {
                 continue;
